c0b201100560df63c46f5388ab64c464c07f97a6
alazharizqi <alazharisqi@gmail.com>
2022-10-17 11:45:43 +0700

    edit

---
+++ looping/cok.java
@@ -0,0 +1,23 @@
+public class cok {
+    public static void main(String[] args) {
+        
+        for (int i = 188;i >= 11; i--) {
+
+            if (i % 2!=0) {
+                System.out.println(i);
+            }
+
+        }
+
+        System.out.println();
+
+        for (int i = 11;i <= 188; i++) {
+
+            if (i % 2!=0) {
+                System.out.println(i);
+            }
+
+        }
+
+    }    
+}
