c1914719522174326a3a69331faa1ce48f4490ae
alazharizqi <alazharisqi@gmail.com>
2022-10-20 09:04:00 +0700

    add-exercise-meet7

---
+++ looping/doex4.java
@@ -0,0 +1,15 @@
+public class doex4 {
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        do {
+
+            i *= 2;
+
+            System.out.println(i);
+            
+        } while (i < 256);
+
+    }
+}
---
+++ looping/whileex4.java
@@ -0,0 +1,15 @@
+public class whileex4 {
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        while (i < 256) {
+            
+            i *= 2;
+
+            System.out.println(i);
+
+        }
+
+    }
+}
