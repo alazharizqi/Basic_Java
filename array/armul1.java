f117a541c8773ee3b726108ededd78869e280ac8
alazharizqi <alazharisqi@gmail.com>
2022-10-20 11:23:47 +0700

    add-armul1

---
+++ array/armul1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class armul1 {
+    
+    public static void main(String[] args) {
+        
+        int [][] num = {
+            {1,2},
+            {3,4}
+        };
+        
+        System.out.println(Arrays.toString(num[0]));
+        System.out.println(Arrays.toString(num[1]));
+
+        System.out.println();
+
+        System.out.println(Arrays.deepToString(num));
+
+    }
+
+}
